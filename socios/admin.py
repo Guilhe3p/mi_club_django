64c2fab7dd183ad7035a86d931a1cd7212dba324
Guilhe3p <guilhermerutenberg@gmail.com>
2023-11-26 18:04:42 -0300

    admin init

--- socios/admin.py
+++ socios/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django import forms
+from .models import User, Curso, Predio, Actividad, Inscripcion, DiaCurso, Dia
 
-# Register your models here.
+
+#Register your models here.
+admin.site.register(Curso)
+admin.site.register(Predio)
+admin.site.register(Actividad)
+admin.site.register(Inscripcion)
+admin.site.register(DiaCurso)
+admin.site.register(Dia)
+admin.site.register(User)
+
+
+admin.site.index_title = "Tablas"
+admin.site.site_header = "Administración del clu"
+admin.site.site_title = "El super clú"
